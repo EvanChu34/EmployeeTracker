503828a5c4802ec0cd4c9240a30517f378ae5def
Evan Chu <echu34@gmail.com>
2021-02-03 20:03:02 -0500

    added table info

--- employee_trackerdb.sql
+++ employee_trackerdb.sql
@@ -12,14 +12,25 @@ PRIMARY KEY(id)
 
 CREATE TABLE role(
 id INTEGER AUTO_INCREMENT NOT NULL,
-
-
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL, 
+department_id INTEGER NOT NULL,
 PRIMARY KEY(id)
 );
 
 CREATE TABLE employee(
 id INTEGER AUTO_INCREMENT NOT NULL,
-
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER NOT NULL,
 
 PRIMARY KEY(id)
 );
+
+SELECT * FROM department; 
+SELECT * FROM employee; 
+SELECT * FROM role; 
+
+
+
