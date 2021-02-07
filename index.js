71c935dc0fc1502d4a991891f66cc284bcd06258
Evan Chu <echu34@gmail.com>
2021-02-07 16:35:54 -0500

    more to the index

--- index.js
+++ index.js
@@ -37,24 +37,57 @@ function startPrompt(){
     ]).then(function(val){
         switch (val.choice){
             case "View All Employees?":
-                
+                viewAllEmployees();
+            break;
+            case "View All Employee's by roles":
+                viewAllRoles();
+            break;
+            case "View All Employee's by departments":
+                viewAllDepartments();
+            break;
+            case "Add Employee":
+                addEmployee();
+            case "Update Employee":
+                updateEmployee();
+            break;
+            case "Add Role":
+                addRole();
+            break;
+            case "Add Department":
+                addDepartment();
+            break;
         }
     })
 
 }
 
 function viewAllEmployees(){
-
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res);
+    startPrompt();
+    })
 }
 
 
 function viewAllRoles(){
-    
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+    })
 }
 
 
 function viewAllDepartments(){
-    
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+    })
 }
 
 function addEmployee(){
