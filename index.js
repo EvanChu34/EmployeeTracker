a2e85368fbf4e36775daa6a7e38cf3eb205d8cda
Evan Chu <echu34@gmail.com>
2021-02-07 14:21:48 -0500

    added functions to the index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 const cTable = require('console.table');
+const { type } = require("os");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,7 +18,58 @@ connection.connect(function(err) {
 });
 
 function startPrompt(){
-
-
+    inquirer
+    .prompt([
+        {
+            type:"list",
+            message: "What would you like to do",
+            name:"choice",
+            choices:[
+                "View All Employees?", 
+                "View All Employee's By Roles?",
+                "View all Emplyees By Deparments", 
+                "Update Employee",
+                "Add Employee?",
+                "Add Role?",
+                "Add Department?"
+            ]
+        }
+    ]).then(function(val){
+        switch (val.choice){
+            case "View All Employees?":
+                
+        }
+    })
 
 }
+
+function viewAllEmployees(){
+
+}
+
+
+function viewAllRoles(){
+    
+}
+
+
+function viewAllDepartments(){
+    
+}
+
+function addEmployee(){
+
+}
+
+function updateEmployee(){
+
+}
+
+function addRole(){
+
+}
+
+function addDepartment(){
+
+}
+
