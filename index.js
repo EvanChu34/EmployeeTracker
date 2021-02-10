37b701351f93790f95c2611514f029181c497045
Evan Chu <echu34@gmail.com>
2021-02-09 22:44:46 -0500

    change to index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Crossfitchuchu34!",// add your password here
+    password: "password", // ADD YOUR OWN PASSWORD HERE!!!
     database: "employee_trackerdb"
 });
 
